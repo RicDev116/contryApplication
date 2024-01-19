c6dbf785b164ee1c1da907765c989950cac20e18
RicDev116 <ricardo_29leyva@outlook.com>
2024-01-19 00:19:05 -0600

    creacion de contact page y nombre de paginas

---
+++ src/app/shared/pages/contactPage/contactPage.component.ts
@@ -0,0 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-contact-page',
+  templateUrl: './contactPage.component.html',
+})
+export class ContactPageComponent { }
